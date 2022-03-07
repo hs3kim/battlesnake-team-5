4d7e0b713e16f1d4c597583d4dc8a966e4650c58
Hans Kim <71810541+hs3kim@users.noreply.github.com>
2022-03-07 18:11:58 +0000

    color

--- src/logic.js
+++ src/logic.js
@@ -43,7 +43,7 @@ function info() {
     const response = {
         apiversion: "1",
         author: "",
-        color: "#063c24",
+        color: "#068c24",
         head: "evil",
         tail: "bolt"
     }
